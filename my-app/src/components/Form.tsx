cf63f4055f1b0dc2a351eb088f87b9591f5d003a
GWAI2018 <>
2022-07-03 06:45:34 +0530

    marking todo

--- my-app/src/components/Form.tsx
+++ my-app/src/components/Form.tsx
@@ -1,23 +1,20 @@
 import { FunctionComponent as FC } from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./todo.scss";
 import Button from "@mui/material/Button";
+import Checkbox from '@mui/material/Checkbox';
 
 const Form: FC = () => {
   const [name, setName] = useState<string>("");
   const [date, setDate] = useState<string>("");
-  const [todo, setTodo] = useState<any>([])
+  const [todo, setTodo] = useState<any>([]);
 
- 
   useEffect(() => {
-   
-    axios
-      .get('http://localhost:5000/api/todo/')
-      .then((res) => {
-         const todo = res.data;
-         setTodo(todo);
-      });
+    axios.get("http://localhost:5000/api/todo/").then((res) => {
+      const todo = res.data;
+      setTodo(todo);
+    });
   }, []);
 
   const handleSubmit = () => {
@@ -25,7 +22,7 @@ const Form: FC = () => {
       name: name,
       date: date,
     };
-   
+
     axios
       .post("http://localhost:5000/api/todo/add", details)
       .then((response) => {
@@ -37,6 +34,13 @@ const Form: FC = () => {
       });
   };
 
+  const Delete = (id: string) => {
+    axios
+      .post("http://localhost:5000/api/todo/delete/" + id)
+      .then((response) => {
+        alert("successfully deleted");
+      });
+  };
   return (
     <div className="container">
       <h1>ToDo List</h1>
@@ -47,7 +51,6 @@ const Form: FC = () => {
           onChange={(e) => setName(e.target.value)}
         />
         <input
-          
           typeof="Date"
           value={date}
           onChange={(e) => setDate(e.target.value)}
@@ -56,21 +59,24 @@ const Form: FC = () => {
           {" "}
           +{" "}
         </Button>
+        
       </form>
       <div>
-        {todo.map((Todo: { name: string; date: string; }) => (
-                  <div>
-                  <strong><p> {Todo.name}</p></strong>
-                  <strong><p> {Todo.date}</p></strong>
-                
-                    
-                    <br></br>
-
-                 
-                      <button className="btn btn-danger" >Delete</button>
-                   
-                      </div>
-                ))}
+        {todo.map((Todo: { name: string; date: string }) => (
+          <div>
+            <Checkbox defaultChecked />
+            <strong>
+              <p> {Todo.name}</p>
+            </strong>
+            <strong>
+              <p> {Todo.date}</p>
+            </strong>
+            <br />
+            <Button variant="contained" onClick={() => Delete(todo._id)}>
+              DELETE
+            </Button>
+          </div>
+        ))}
       </div>
     </div>
   );
